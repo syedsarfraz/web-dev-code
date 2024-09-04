3a767561f9c2d2a2547cdc9b171343fff43997dd
Ahmed Raza <hafizahmedmoon@gmail.com>
2024-09-04 16:49:47 +0500

    add products btn to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CounterComponent } from "../counter/counter.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CounterComponent],
+  imports: [CounterComponent, RouterModule],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
