2a71f4b3b7fb1b4e58339e700bf91ca0697f4d00
Ahmed Raza <hafizahmedmoon@gmail.com>
2024-09-07 21:42:53 +0500

    add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./projects/**/*.{ts,html}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
 
