8abecf356cce7022d2f341bb86d12d35c5034699
Ahmed Raza <hafizahmedmoon@gmail.com>
2024-10-03 16:47:14 +0500

    add dynamic product and variants in product form

--- projects/practice/src/app/shopping/add-product/add-product.component.ts
+++ projects/practice/src/app/shopping/add-product/add-product.component.ts
@@ -1,8 +1,54 @@
 import { Component } from '@angular/core';
-import { ProductListComponent } from '../product-list/product-list.component';
+import { autoId } from '../../auto-id';
+import { ModelDirective } from '../../app-model.directive';
 
 @Component({
   templateUrl: './add-product.component.html',
   standalone: true,
+  imports: [ModelDirective],
 })
-export class AddProductComponent {}
+export class AddProductComponent {
+  variants: { id: string; name: string }[] = [];
+
+  productVariants: {
+    id: string;
+    quantity: string;
+    price: string;
+    variantMap: Record<string, string>;
+  }[] = [];
+
+  constructor() {
+    this.addProductRow();
+  }
+
+  addVariant() {
+    const id = autoId();
+    this.variants.push({ id, name: '' });
+    this.productVariants.forEach((product) => (product.variantMap[id] = ''));
+  }
+  removeVariant(index: number) {
+    const [item] = this.variants.splice(index, 1);
+    this.productVariants.forEach(
+      (product) => delete product.variantMap[item.id]
+    );
+  }
+
+  addProductRow() {
+    this.productVariants.push({
+      id: autoId(),
+      quantity: '',
+      price: '',
+      variantMap: this.variants.reduce((map, variant) => {
+        map[variant.id] = '';
+        return map;
+      }, {} as Record<string, string>),
+    });
+  }
+  removeProductRow(index: number) {
+    this.productVariants.splice(index, 1);
+  }
+
+  saveProduct() {
+    console.log(this.productVariants);
+  }
+}
