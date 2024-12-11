6e07861a58233367072e81c548f1e33bcc19d519
Ahmed Raza <hafizahmedmoon@gmail.com>
2024-12-11 17:28:05 +0500

    initial commit

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,19 @@
+import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { accessibility, boat, person } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+  standalone: true,
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+})
+export class TabsPage {
+  public environmentInjector = inject(EnvironmentInjector);
+
+  constructor() {
+    addIcons({accessibility,person,boat});
+  }
+}
