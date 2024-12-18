d70a68afdf83a0f0ecf378a771472c80a1ebfa41
Ahmed Raza <hafizahmedmoon@gmail.com>
2024-12-18 15:02:37 +0500

    add todo list with toggle and remove

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
+  AlertController,
+  IonButton,
+  IonCheckbox,
   IonContent,
   IonHeader,
+  IonIcon,
   IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
   IonList,
   IonTitle,
   IonToolbar,
-  IonLabel,
-  IonButton,
-  IonIcon, IonCheckbox } from '@ionic/angular/standalone';
+  IonAlert,
+  IonLoading, IonToast } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { checkmarkCircle } from 'ionicons/icons';
 
@@ -26,7 +38,13 @@ interface Todo {
   styleUrls: ['./home.page.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [IonCheckbox,
+  imports: [IonToast,
+    IonLoading,
+    IonAlert,
+    IonItemOptions,
+    IonItemSliding,
+    IonItemOption,
+    IonCheckbox,
     IonIcon,
     IonButton,
     IonCheckbox,
@@ -44,27 +62,84 @@ interface Todo {
 export class HomePage {
   todos = signal<Todo[]>([]);
 
+  error = signal('');
+  loading = signal(false);
+
+  // removeTodoAlert = signal<Todo | null >(null)
+
+  alertCtrl = inject(AlertController);
+
   constructor() {
     this.getTodos();
     addIcons({ checkmarkCircle });
   }
 
   async getTodos() {
+    this.loading.set(true);
     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    this.loading.set(false);
 
     const todos: Todo[] = await res.json();
 
     this.todos.set(todos);
   }
 
-  toggle(todo: Todo) {
+  async toggle(todo: Todo) {
+    const updatedTodo = { ...todo, completed: !todo.completed };
+    this.loading.set(true);
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedTodo),
+    }).catch((e) => {
+      this.error.set('Failed to toggle todo')
+      throw e
+    }).finally(() => this.loading.set(false));
     this.todos.update((todos) =>
       todos.map((_todo) => {
         if (_todo === todo) {
-          return { ...todo, completed: !todo.completed };
+          return updatedTodo;
+          // return { ...todo, completed: !todo.completed };
         }
         return _todo;
       })
     );
+  }
+
+  async remove(todo: Todo) {
+    const alert = await this.alertCtrl.create({
+      message: `Are you sure, you want to delete "${todo.title}"`,
+      // buttons: ['Cancel', { text: 'OK', role: 'destructive' }],
+      buttons: [
+        'Cancel',
+        {
+          text: 'OK',
+          handler: async () => {
+            this.loading.set(true);
+            await fetch(
+              `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+              {
+                method: 'DELETE',
+              }
+            );
+            this.loading.set(false);
+            this.todos.update((todos) =>
+              todos.filter((_todo) => todo !== _todo)
+            );
+          },
+        },
+      ],
+      backdropDismiss: false,
+    });
+    alert.present();
+
+    // const event = await alert.onDidDismiss();
+    // if (event.role === 'destructive') {
+    //   this.todos.update((todos) => todos.filter((_todo) => todo !== _todo));
+    // }
+  }
+
+  removeTodo(event: any, todo: Todo) {
+    if (event.role !== 'destructive') return;
+    this.todos.update((todos) => todos.filter((_todo) => todo !== _todo));
   }
 }
